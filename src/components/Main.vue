89153154b63e527215e3ef7965a28da4b8e1be5a
Tiago Brito <gtiago.brito@hotmail.com>
2021-01-04 12:39:39 -0300

    Game Base finish

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,10 +3,12 @@
     <div class="screen-block">
       <h1>Vire o celular</h1>
     </div>
-    <div class="menu" v-if="!running">
+    <div class="menu" v-if="!running && !hasResult">
       <div class="panel buttons">
-        <input type="text" v-model="player.name">
-        <button @click="startGame" class="btn btn__new-game"><span>Start</span></button>
+        <div class="grid-container">
+          <input type="text" v-model="player.name">
+          <button @click="startGame" class="btn btn__new-game"><span>Start</span></button>
+        </div>
       </div>
     </div>
     <div class="game" v-else>
@@ -23,9 +25,9 @@
       </div>
       <div class="panel buttons">
         <template>
-          <button @click="attack(false)" class="btn btn__attack" :disabled="actions"><span>Ataque</span></button>
-          <button @click="attack(true)" class="btn btn__special-attack" :disabled="actions"><span>Ataque Especial</span></button>
-          <button @click="healAndHurt" class="btn btn__heal" :disabled="actions"><span>Curar</span></button>
+          <button @click="doAttack(false)" class="btn btn__attack" :disabled="actions"><span>Ataque</span></button>
+          <button @click="doAttack(true)" class="btn btn__special-attack" :disabled="actions"><span>Ataque Especial</span></button>
+          <button @click="doHealAndHurt" class="btn btn__heal" :disabled="actions"><span>Curar</span></button>
           <button @click="running = false" class="btn btn__give-up"><span>Desistir</span></button>
         </template>
         
@@ -54,6 +56,8 @@ export default {
   },
   data() {
     return {
+      menuSound: new Audio('/audio/menu.mp3'),
+      battleSound: new Audio('/audio/battle.mp3'),
       running: false,
       actions: false,
       fired: false,
@@ -72,6 +76,9 @@ export default {
       }
     };
   },
+  mounted () {
+    this.menuSound.play()
+  },
   computed: {
     hasResult() {
       return this.player.life == 0 || this.monster.life == 0
@@ -79,6 +86,8 @@ export default {
   },
   methods: {
     startGame() {
+      this.menuSound.pause()
+      this.battleSound.play()
       this.running = true
       this.monster.life = 100
       this.monster.status = 'iddle'
@@ -86,47 +95,47 @@ export default {
       this.player.status = 'iddle'
       this.logs = []
     },
-    attack(especial) {
+    doAttack(especial) {
       this.doAtcion()
-      this.hurt("monster", 5, 10, especial, this.player.name, this.monster.name, "player")
+      this.doHurt("monster", 5, 10, especial, this.player.name, this.monster.name, "player")
       this.doPlayerAttack(especial)
       setTimeout(() => {
         if (this.monster.life > 0) {
           this.doMonsterAttack()
-          this.hurt('player', 7, 12, false, this.monster.name, this.player.name, "monster")
+          this.doHurt('player', 7, 12, false, this.monster.name, this.player.name, "monster")
         }
       }, 2000);
     },
-    hurt(prop, min, max, especial, source, target, cls) {
+    doHurt(prop, min, max, especial, source, target, cls) {
       const plus = especial ? 5 : 0
       const hurt = this.getRandom(min + plus, max + plus)
       setTimeout(() => {
         this[prop].life = Math.max(this[prop].life - hurt, 0)
         this.doShowHurt(prop, hurt)
-        this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
+        this.doRegisterLog(`${source} atingiu ${target} com ${hurt}.`, cls)
       }, 1200);
     },
-    healAndHurt() {
+    doHealAndHurt() {
       this.doAtcion()
-      this.heal(10, 15)
+      this.doHeal(10, 15)
       setTimeout(() => {
         this.doMonsterAttack()
-        this.hurt('player', 7, 12, false, this.monster.name, this.player.name, "monster")
+        this.doHurt('player', 7, 12, false, this.monster.name, this.player.name, "monster")
       }, 2000);
     },
-    heal(min, max) {
+    doHeal(min, max) {
       this.player.status = 'healer'
       this.doPlayerIddle(1200);
       const heal = this.getRandom(min, max)
       this.player.life = Math.min(this.player.life + heal, 100)
       this.doShowHurt('heal', heal)
-      this.registerLog(`${this.player.name} ganhou ${heal} de vida.`, "player")
+      this.doRegisterLog(`${this.player.name} ganhou ${heal} de vida.`, "player")
     },
     getRandom(min, max) {
       const value = Math.random() * (max - min) + min
       return Math.round(value)
     },
-    registerLog(text, cls) {
+    doRegisterLog(text, cls) {
       this.logs.unshift({ text, cls })
     },
     doMonsterAttack () {
@@ -134,6 +143,7 @@ export default {
       this.fired = true
       this.monster.status = 'attack'
       this.player.status = 'hurt'
+      this.doPlaySound('dragon_breath_fire')
 
       this.doMonsterIddle(720);
 
@@ -145,6 +155,7 @@ export default {
       this.player.status = (special) ? 'special-attack' : 'attack'
       this.monster.status = 'hurt'
       this.doPlayerIddle(1200);
+      this.doPlaySound('hit')
     },
     doMonsterIddle (time) {
       setTimeout(() => {
@@ -173,12 +184,17 @@ export default {
       setTimeout(() => {
         this[target].damage = ''
       }, 1000);
-    }
+    },
+    doPlaySound(sound) {
+      let audioPlay = new Audio('/audio/'+sound+'.mp3')
+			audioPlay.play()
+		}
   },
   watch: {
     hasResult(value) {
       if (this.monster.life === 0) {
         this.monster.status = 'death'
+        this.doPlaySound('winner')
       }
 
       if (this.player.life === 0) {
